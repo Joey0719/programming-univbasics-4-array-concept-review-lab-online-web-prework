0ecf46fc95ecfc6fc1d72fbc99afa6afc111355a
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-04 13:09:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,4 +9,4 @@ describe 'finding data'
   valley = ['5','4','3','2','1','0','1','2','3','4','5']
   zig_zag = ['500','4','1000','5','250']
   
-  describe 'find_element_index(array,)
+  describe 'find_element_index(array, value_to_find)
